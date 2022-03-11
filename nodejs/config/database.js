26657232ba4e1f6dc441c5f38373470ef2e3d678
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-03-12 01:29:38 +0200

    Create Database

--- nodejs/config/database.js
+++ nodejs/config/database.js
@@ -1,12 +1,20 @@
-var sequelize = new Sequelize('database', 'username', 'password', {
-    host: 'localhost',
-    dialect: 'mysql',
-  
-    pool: {
-      max: 5,
-      min: 0,
-      idle: 10000
-    },
-  
-  });
-  
+const config = require('./config.json');
+const mysql = require('mysql2/promise');
+const { Sequelize } = require('sequelize');
+
+module.exports = db = {};
+
+initialize();
+
+async function initialize() {
+    // create db if it doesn't already exist
+    const { host, port, user, password, database } = config.development;
+    const connection = await mysql.createConnection({ host, port, user, password });
+    await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
+
+    // connect to db
+    const sequelize = new Sequelize(database, user, password, { dialect: 'mysql' });
+
+    // sync all models with database
+    await sequelize.sync();
+}
