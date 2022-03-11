d544815da37f49001606e229f00612dcdf676694
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-03-12 01:26:22 +0200

    Add server

---
+++ nodejs/server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
